3cfcb1327227bbe1e8d2fd71570526583e56378d
dumi994 <dumi.9944@gmail.com>
2021-07-30 18:16:45 +0200

    tentativo inserimento tag e cat fail

--- resources/views/guests/about.blade.php
+++ resources/views/guests/about.blade.php
@@ -7,9 +7,12 @@
 <div class="card text-left" v-for="post in posts">
     <img src="holder.js/100px180/" alt="" class="card-img-top">
     <div class="card-body">
+        
         <h3 class="card-title">@{{post.title}}</h3>
         <p class="card-text">@{{post.body}}</p>
         <p class="card-text">Tag: @{{post.tag}}</p>
+        <p class="card-text">Category: @{{post.tag}}</p>
+
     </div>
 </div>
 @endsection
