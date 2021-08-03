2d4f69c0d6f2ee6e25ac4174119618af8c2908ad
dumi994 <dumi.9944@gmail.com>
2021-08-03 17:58:12 +0200

    aggiunto componente in about

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <h2>Test Component</h2>
+        <p>Detailed information about posts</p>
+        <p>{{text}}</p>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['text'],
+        mounted() {
+            console.log('Component mounted.')
+            
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .container{
+        margin:0;
+        text-align:left;
+        padding:10px;
+        
+        
+    }
+ </style>
--- resources/views/guests/about.blade.php
+++ resources/views/guests/about.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 
-<h1> SPA Blog</h1>
+<h1> About posts</h1>
+<post-component></post-component>
+<!-- <post-component text="lorem1"></post-component> -->
 
 <div class="card text-left" v-for="post in posts">
     <img src="holder.js/100px180/" alt="" class="card-img-top">
@@ -10,8 +12,8 @@
         
         <h3 class="card-title">@{{post.title}}</h3>
         <p class="card-text">@{{post.body}}</p>
+        <p class="card-text">Category: @{{post.category.name}}</p>
         <p class="card-text">Tag: @{{post.tag}}</p>
-        <p class="card-text">Category: @{{post.tag}}</p>
 
     </div>
 </div>
